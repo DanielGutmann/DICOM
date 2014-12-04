47e375b802283005e2ac08a393633cae67e3ef54
Agnieszka <agnieszka_szczotka@onet.pl>
2014-12-04 21:10:59 +0100

    LoG kernel in 2D and 3D

--- LoGKernel.py
+++ LoGKernel.py
@@ -7,32 +7,50 @@ import matplotlib.pyplot as plt
 import numpy as np
 # LoG=LoGcontatnt*(r^2/sigma^2 -3)*e^-r^2/(2*sigma^2) ,where r is in [mm]
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-
-x, y, z = np.zeros((3, 3, 3))
-sigma = 2
 class LoG(object):
-    def __init__(self):
+    def __init__(self,sigma):
+        three_sigma= 3.0*sigma
+        self.sigma=sigma
+        x = np.arange(-three_sigma,three_sigma, 0.1)
+        y = np.arange(-three_sigma,three_sigma, 0.1)
+        z = np.arange(-three_sigma,three_sigma, 0.1)
 
-        x = np.arange(-100, 100, 0.5)
-        y = np.arange(-100, 100, 0.5)
-        z = np.arange(-100, 100, 0.5)
-        self.X, self.Y, self.Z = np.meshgrid(x, y ,z)
+        self.X, self.Y, self.Z= np.meshgrid(x, y,z )
+
 
 
     def __call__(self, sigma):
 
-        sigma_square = sigma ** 2
-        LoGcontant = 1.0 / (2.0 * pi * sigma_square ** 2)
-        R_square = x ** 2 + y ** 2 + z ** 2
-        LoG = LoGcontant * (R_square / sigma_square - 3) * np.exp(-R_square / 2 * sigma_square)
-        LoG_normalized = LoG / np.sum(LoG)
+        sigma_square = self.sigma ** 2
+        LoGcontant = 1.0 / (2.0 * pi * (sigma_square ** 2))
+        R_square = self.X ** 2 + self.Y ** 2+self.Z ** 2
+
+        exponetnta=np.exp(-R_square /(2 * sigma_square))
+        LoG = LoGcontant * (R_square / sigma_square - 2) * exponetnta
+        LoG_normalized = LoG/np.sum(LoG)
+
         return LoG_normalized
 
 
+class LoG2D(object):
+    def __init__(self,sigma):
+        three_sigma= 3.0*sigma
+        self.sigma=sigma
+        x = np.arange(-three_sigma,three_sigma, 0.1)
+        y = np.arange(-three_sigma,three_sigma, 0.1)
 
-surf = ax.plot_surface(X, Y, k, rstride=1, cstride=1, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
 
-plt.show()
+        self.X, self.Y= np.meshgrid(x, y )
+
+
+    def __call__(self):
+
+        sigma_square = self.sigma ** 2
+        LoGcontant = 1.0 / (2.0 * pi * (sigma_square ** 2))
+        R_square = self.X ** 2 + self.Y ** 2
+
+        exponetnta=np.exp(-R_square /(2 * sigma_square))
+        LoG = LoGcontant * (R_square / sigma_square - 2) * exponetnta
+        LoG_normalized = LoG/np.sum(LoG)
+        print np.sum(LoG_normalized)
+        return LoG_normalized
--- test/test_LoGKernel.py
+++ test/test_LoGKernel.py
@@ -1,8 +1,23 @@
+from matplotlib import cm
+
 __author__ = 'Agnieszka'
 
 import unittest
-from
+from LoGKernel import LoG2D
+from matplotlib.pyplot import figure, show
+
 
 class readDicomTest(unittest.TestCase):
 
     def setUp(self):
+        sigma= 60
+        self.log=LoG2D(sigma)
+
+    def test_visualization(self):
+
+        fig = figure()
+        ax = fig.gca(projection='3d')
+        surf = ax.plot_surface(self.log.X, self.log.Y, self.log(), rstride=1, cstride=1, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+        show()
